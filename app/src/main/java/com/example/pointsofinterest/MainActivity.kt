378fa869518559a5648160130676a620d854ac73
Grahame White <4whitg16@solent.ac.uk>
2021-05-09 12:58:57 +0100

    Added navigation drawer

--- app/src/main/java/com/example/pointsofinterest/MainActivity.kt
+++ app/src/main/java/com/example/pointsofinterest/MainActivity.kt
@@ -2,16 +2,33 @@ package com.example.pointsofinterest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.preference.PreferenceManager
+import com.example.pointsofinterest.databinding.ActivityMainBinding
 import org.osmdroid.config.Configuration
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
         // This line sets the user agent, a requirement to download OSM maps
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+    }
 
-        setContentView(R.layout.activity_main)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
